a9f41227dc37b673217cbe925711d3dace955249
Imangali.K <79092624+Imangali-ang@users.noreply.github.com>
2023-05-24 20:47:02 +0600

    fix login by Company

--- src/main/java/org/example/controller/CompanyController.java
+++ src/main/java/org/example/controller/CompanyController.java
@@ -3,8 +3,10 @@ package org.example.controller;
 import org.example.Exceptions.models.BasicException;
 import org.example.model.Company;
 import org.example.model.Intern;
+import org.example.model.User;
 import org.example.model.Vacancy;
 import org.example.repository.CompanyRepository;
+import org.example.repository.UserRepository;
 import org.example.service.VacancyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -19,9 +21,14 @@ public class CompanyController {
     @Autowired
     private VacancyService vacancyService;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/{companyId}")
     public Company getProfile(@PathVariable("companyId") Long id){
-        Company company = companyRepository.findById(id)
+        User user = userRepository.findByUserId(id)
+                .orElseThrow(() -> new BasicException("CAN'T FIND INTERN WITH THIS ID"));
+        Company company = companyRepository.findByCompanyNumber(user.getUserNumber())
                 .orElseThrow(() -> new BasicException("CAN'T FIND COMPANY WITH THIS ID"));
         return company;
     }
--- src/main/java/org/example/repository/CompanyRepository.java
+++ src/main/java/org/example/repository/CompanyRepository.java
@@ -1,9 +1,16 @@
 package org.example.repository;
 
 import org.example.model.Company;
+import org.example.model.Intern;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
+
+    @Query("SELECT c FROM Company c WHERE c.contacts = :userNumber")
+    Optional<Company> findByCompanyNumber(String userNumber);
 }
